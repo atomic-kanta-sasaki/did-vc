1ceac2495702a47826595fc0b9630bbc64f50daf
atomic-kanta-sasaki <kanta.sasaki.0111@gmail.com>
2024-10-12 22:21:48 +0900

    exec prettier

--- web/src/AppRoutes.tsx
+++ web/src/AppRoutes.tsx
@@ -1,18 +1,14 @@
-import {
-    BrowserRouter,
-    Route,
-    Routes
-  } from "react-router-dom";
-  import { Issue } from "./pages/Issue";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Issue } from "./pages/Issue";
 
-  const AppRoutes = () => {
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Issue />} />
-        </Routes>
-      </BrowserRouter>
-    )
-  }
-  
-  export default AppRoutes;
+const AppRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Issue />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRoutes;
--- web/src/pages/Verify/index.tsx
+++ web/src/pages/Verify/index.tsx
@@ -1,33 +1,39 @@
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import { useState } from 'react';
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { useState } from "react";
 
 export const VefiryVC: React.FC = () => {
-
-    const [vc, setVc] = useState('');
-    const handleVcChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setVc(e.target.value);
-    }
-    const handleSumit = async () => {
-        const res = await fetch('http://localhost:3000/verifyVc', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                vc
-            })
-        });
-        console.log(await res.json());
-    }
-    return (
-        <div>
-          <h1>VC検証</h1>
-          <Box sx={{ width: 500, maxWidth: '100%' }}>
-            <TextField fullWidth label="VC" id="fullWidth" onChange={handleVcChange}/>
-            <Button onClick={handleSumit} variant="contained">送信</Button>
-          </Box>
-        </div>
-    )
-}
+  const [vc, setVc] = useState("");
+  const handleVcChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setVc(e.target.value);
+  };
+  const handleSumit = async () => {
+    const res = await fetch("http://localhost:3000/verifyVc", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        vc,
+      }),
+    });
+    console.log(await res.json());
+  };
+  return (
+    <div>
+      <h1>VC検証</h1>
+      <Box sx={{ width: 500, maxWidth: "100%" }}>
+        <TextField
+          fullWidth
+          label="VC"
+          id="fullWidth"
+          onChange={handleVcChange}
+        />
+        <Button onClick={handleSumit} variant="contained">
+          送信
+        </Button>
+      </Box>
+    </div>
+  );
+};
